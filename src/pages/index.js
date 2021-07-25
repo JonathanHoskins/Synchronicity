2d6944fec7859e93e7466c8f70abbea5c69f79fe
Jonathan Hoskins <jonison06@gmail.com>
2021-07-25 19:02:33 -0400

    fixed link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import logo from '/src/images/Synchronicityflyer.png'
+import { Link } from 'gatsby'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
